16d1ea48a6046a3e491b347bddf51ed291088425
SilasCloud <silas.ajayi9@gmail.com>
2025-03-22 01:39:15 +0000

    Update Terraform configuration for S3 backend and variables

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
-    key    = "terraform.tfstate"
-    region = "us-east-2"
+    bucket = "vproaction2025"
+    key    = "terraform.tfstate" #A centeralized state file
+    region = "us-east-1"
   }
 
   required_version = "~> 1.6.3"
